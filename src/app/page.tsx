0fddb380def38a0dc2762662423f226f8bffcd70
AcWrong02 <3024800887@qq.com>
2025-12-29 21:35:48 +0800

    feat(ui): 引入拖拽组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+import {
+	ResizableHandle,
+	ResizablePanel,
+	ResizablePanelGroup
+} from "@/components/ui/resizable";
 import Tiptap from "@/features/editor/Tiptap";
 
 export default function Home() {
 	return (
-		<div className="h-screen w-screen overflow-auto bg-gray-50 flex justify-center items-start">
-			<Tiptap />
+		<div className="h-screen w-full">
+			<ResizablePanelGroup direction="horizontal" className="h-full w-full">
+				<ResizablePanel defaultSize={25} maxSize={30} minSize={15}>
+					<div className="flex h-full items-center justify-center p-6">
+						<span className="font-semibold">Sidebar</span>
+					</div>
+				</ResizablePanel>
+				<ResizableHandle withHandle />
+				<ResizablePanel defaultSize={75}>
+					<Tiptap />
+				</ResizablePanel>
+			</ResizablePanelGroup>
 		</div>
 	);
 }
